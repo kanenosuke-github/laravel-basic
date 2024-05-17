0ae96494425391e72b01488ee92ae134893e5107
kanenosuke-github <shun.216.nosuke@gmail.com>
2024-05-18 08:54:30 +0900

    laravel-lesson-15

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 // ルーティングを設定するコントローラを宣言する
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\ProductController;
 
 
 /*
@@ -22,3 +23,6 @@ Route::get('/', function () {
 });
 
 Route::get('/hello', [HelloController::class,'index']);
+
+Route::get('/products', [ProductController::class, 'index']);
+
