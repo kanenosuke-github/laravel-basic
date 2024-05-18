bb5f4815b2983416bb8e61d84f8d2f1d2daa6e42
kanenosuke-github <shun.216.nosuke@gmail.com>
2024-05-19 08:31:05 +0900

    laravel-eloquent

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 // ルーティングを設定するコントローラを宣言する
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VendorController;
 
 
 /*
@@ -26,3 +27,6 @@ Route::get('/hello', [HelloController::class,'index']);
 
 Route::get('/products', [ProductController::class, 'index']);
 
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::get('/vendors/{id}', [VendorController::class, 'show']);
