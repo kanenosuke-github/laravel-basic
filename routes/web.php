983d6bef7a98b19012eec606fd4aaf807e5fa880
kanenosuke-github <shun.216.nosuke@gmail.com>
2024-05-24 10:38:33 +0900

    laravel-errors

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\VendorController;
-
+use App\Http\Controllers\RequestController;
+use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\SignInController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +29,23 @@ Route::get('/hello', [HelloController::class,'index']);
 
 Route::get('/products', [ProductController::class, 'index']);
 
+Route::get('/products/create', [ProductController::class, 'create']);
+ 
+ Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+
 Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/vendors/create', [VendorController::class, 'create']);
+ 
+ Route::post('/vendors/store', [VendorController::class, 'store'])->name('vendors.store');
 
 Route::get('/vendors/{id}', [VendorController::class, 'show']);
+
+Route::get('/requests/create', [RequestController::class, 'create']);
+ 
+Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
+
+Route::get('/responses', [ResponseController::class, 'index']);
+
+Route::get('/sign-in', [SignInController::class, 'create']);
+ 
+ Route::post('/sign-in', [SignInController::class, 'store'])->name('sign-in.store');
